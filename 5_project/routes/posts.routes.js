d896bb3421468817473e575d7c57c6bbb6aba2ff
dahye24 <113884373+dahye24@users.noreply.github.com>
2022-10-18 20:39:12 +0900

    10/18 5주차 팀플

---
+++ 5_project/routes/posts.routes.js
@@ -0,0 +1,14 @@
+// routes/posts.routes.js
+const express = require('express');
+const router = express.Router();
+
+const PostsController = require('../controllers/posts.controller');
+const postsController = new PostsController();
+
+// 게시글 검색 싹다
+router.get('/', postsController.getPosts);
+
+//게시글 포스팅
+router.post('/', postsController.createPost);
+
+module.exports = router;
